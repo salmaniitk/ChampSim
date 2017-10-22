aa1e59a0d9c77338dfa6777fd8a923d922edee72
mohdsalma@cse.iitk.ac.in <mohdsalman@cse.iitk.ac.in>
2017-10-22 11:50:18 +0530

    Inclusive files

---
+++ obj/src/uncore.d
@@ -0,0 +1,16 @@
+obj/src/uncore.o: src/uncore.cc inc/uncore.h inc/champsim.h inc/cache.h \
+ inc/memory_class.h inc/block.h inc/instruction.h inc/dram_controller.h
+
+inc/uncore.h:
+
+inc/champsim.h:
+
+inc/cache.h:
+
+inc/memory_class.h:
+
+inc/block.h:
+
+inc/instruction.h:
+
+inc/dram_controller.h:
